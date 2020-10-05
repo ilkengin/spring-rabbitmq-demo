5310aba4cfb00b567692e55b16ea2181cf4d44e3
Ilkan Engin <ilkan.engin@siemens.com>
2020-10-05 12:19:26 +0300

    Added get method for sending message

---
+++ messaging-rabbitmq/src/main/java/com/ilkengin/messagingrabbitmq/controller/SendMessageController.java
@@ -0,0 +1,30 @@
+package com.ilkengin.messagingrabbitmq.controller;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ilkengin.messagingrabbitmq.config.RabbitConfig;
+
+@RestController
+@RequestMapping(value = { "/send" })
+public class SendMessageController {
+
+	@Autowired
+	private RabbitTemplate rabbitTemplate;
+	
+	@GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public String sendDefaultMessage()
+    {
+		System.out.println("Sending message...");
+        rabbitTemplate.convertAndSend(RabbitConfig.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
+        return "SENT";
+    }
+}
