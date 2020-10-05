19a60a3f1a882222cb9c69e3ebdd23f0e4301330
Ilkan Engin <ilkan.engin@siemens.com>
2020-10-05 09:25:57 +0300

    Added rabbit MQ spring project

---
+++ messaging-rabbitmq/src/test/java/com/ilkengin/messagingrabbitmq/MessagingRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilkengin.messagingrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
